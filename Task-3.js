a448be136e4b71b5a3e2895a016a46a894c26a26
d0s <d0s>
2024-11-13 20:29:06 +0700

    Task-3

---
+++ Task-3.js
@@ -0,0 +1,12 @@
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8]
+chunkArray(array, 2)
+function chunkArray(array, size) {
+  const newArray = []
+  // создаем переменную куда будем складвыть нарезаные массивы
+  for (let i = 0; i < array.length; i + size) {
+    newArray.push(array.splice(i, size))
+    // вырезаем массив и сразу пушим в новый массив
+  }
+  console.log(newArray);
+}
