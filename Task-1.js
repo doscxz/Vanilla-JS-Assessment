e66a6c3aedf6c6c2d7810d83c614dcd7bb222817
d0s <d0s>
2024-11-13 17:41:26 +0700

    task 1 отредактировал комментарии

--- Task-1.js
+++ Task-1.js
@@ -7,14 +7,16 @@ console.log(isPalindrome(str2));
 function isPalindrome(str) {
   const reverse = str.toLowerCase().split('')
   const string = str.toLowerCase().split('')
-  // создаем 2 переменные приводим к общему виду
-  // создаем 2 т.к. в дальнейшем будем их сравнивать и 1 будет реверснута
-  // другая остоваться как есть
+  /**
+   * создаем 2 переменные приводми в нижний регистр и сплитим
+   * что бы в дальнейшем убрать пробелы и 1 реверснуть
+   */
   reverse.reverse()
   // реверсаем 1 из переменыx
   if (deleteSpaces(reverse).join('') === deleteSpaces(string).join('')) {
-    //здесь решил создать функицю котораю убирает все пробелы что бы сравнение получилось точным
-    // вынес ее что бы не переписывать по 2 раза
+    /**
+     * здесь вынес функцию которая убирает пробелы, что бы не переписывать по 2 раза
+     */
     return true
     // возвращаем true если строчки равны
   } else {
@@ -24,12 +26,15 @@ function isPalindrome(str) {
 }
 
 function deleteSpaces(array) {
-  array.filter(e => {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === ' ') {
-        array.splice(i, 1)
-      }
+  // принимаем 1 из массивов
+  for (let i = 0; i < array.length; i++) {
+    // пробегаем по его элементам
+    if (array[i] === ' ') {
+      // и если находим 'пробел' удалаем его
+      array.splice(i, 1)
+
     }
-  })
+  }
   return array
+  // возвращаем получившийся массив
 }
